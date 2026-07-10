c98771a08d0adeb8423b93c970cb3020f554ce73
jaganpari <jaganpari@gmail.com>
2026-07-10 18:01:00 +0530

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { Login } from './features/login/login';
 
-export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
-];
+export const routes: Routes = [];
