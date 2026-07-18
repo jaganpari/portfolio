f788b7cbd1712b4649f15596d6a3367296d73a27
jaganpari <jaganpari@gmail.com>
2026-07-18 07:45:06 +0530

    CSS and Text changes

--- src/app/sections/experience/experience.ts
+++ src/app/sections/experience/experience.ts
@@ -29,43 +29,68 @@ export class Experiences {
     {
       period: 'Aug. 2025 – May 2026',
       title: 'Application Developer',
-      company: 'ICON plc, Chennai One IT SEZ, Chennai',
+      company: 'ICON plc, Chennai One IT SEZ, Chennai.',
       description:
-        'Developing scalable Angular applications using Standalone Components, Signals, RxJS, Angular Material and reusable UI architecture.',
+        'Developed a dynamic C-SSRS assessment module using Angular Reactive Forms.',
       technologies: [
-        'Angular',
+        'Angular 19',
         'Signals',
         'RxJS',
         'Angular Material',
-        'TypeScript'
+        'TypeScript',
+        'NgRx'
       ]
     },
 
     {
       period: 'Jan 2025 – Jun 2025',
       title: 'Senior Technical Specialist',
-      company: 'iLink Digital Private Limited, RMZ Millenia Park-II, Chennai',
+      company: 'iLink Digital Private Limited, RMZ Millenia Park-II, Chennai.',
       description:
-        'Built enterprise applications with Reactive Forms, REST APIs, authentication, dashboards and responsive design.',
+        'Worked Micro Frontend architecture using Nx and Module Federation to manage multiple applications.',
       technologies: [
-        'Angular',
+        'Angular 17',
         'RxJS',
         'Bootstrap',
-        'REST API'
+        'Akita',
+        'Nx'
       ]
     },
 
     {
       period: 'Aug. 2019 – Jan. 2025',
       title: 'Senior Software Developer',
-      company: 'Video Ad-Tech',
+      company: 'Sify Technologies Limited, Tidel Park, Chennai.',
       description:
-        'Migrated legacy AngularJS applications to Angular while improving performance and maintainability.',
+        'Upgraded application Angular 7 to Angular 14 version.',
       technologies: [
-        'AngularJS',
-        'Angular',
-        'JavaScript',
-        'SCSS'
+        'Angular 14',
+        'NgRx',
+        'RxJs',
+        'Angulatr Material'
+      ]
+    },
+    {
+      period: 'Jul. 2017 - May 2019',
+      title: 'Software Engineer Level 2',
+      company: 'YuMe India Private Limited, Ascendas IT Park, Chennai.',
+      description:
+        'Refactored legacy code to modern Angular standards in Angular 7.',
+      technologies: [
+        'Angular 7',
+        'Bootstrap',
+        'RxJs'
+      ]
+    },
+    {
+      period: 'Jun. 2014 - Jul. 2017',
+      title: 'Senior UI Developer',
+      company: 'Convergent Technology Solutions Private Limited, Chennai.',
+      description:
+        'Developed Single Page Applications (SPAs) using AngularJS.',
+      technologies: [
+        'Angular JS',
+        'Bootstrap'
       ]
     }
 
