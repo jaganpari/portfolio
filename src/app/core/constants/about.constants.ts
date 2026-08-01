5157e10f58156b8c9112c65a9a75b58bf233259f
jaganpari <jaganpari@gmail.com>
2026-08-01 09:44:34 +0530

    Search implemented in Skilla

--- src/app/core/constants/about.constants.ts
+++ src/app/core/constants/about.constants.ts
@@ -1,8 +1,8 @@
 export const HIGHLIGHTED_TECHNOLOGIES: string[] = [
-  '11.5+ Years Experience',
-  'Angular 4 - Angular 19',
-  'NgRx & State Management',
-  'Angular Material',
-  'RxJS & Signals',
-  'Enterprise Applications'
+  'Angular JS, Angular 4 - Angular 19',
+  'JavaScript (ES6+), TypeScript',
+  'NgRx, RxJS, Nx, HTML5, CSS3, SCSS',
+  'Angular Material, Bootstrap, Ant Design',
+  'Standalone Components, Signals, Akita',
+  'Jest, Jasmine + Karma, Vitest'
 ];
--- src/app/sections/skills/skills.ts
+++ src/app/sections/skills/skills.ts
@@ -3,18 +3,40 @@ import { CommonModule } from '@angular/common';
 import { FRONT_END_SKILLS, TECHNOLOGIES } from '../../core/constants/skills.constants';
 import { ChipsComponent } from '../../shared/ui/chips-component/chips-component';
 import { SkillLevelPipe } from '../../shared/pipes/skill-level-pipe';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { startWith } from 'rxjs';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [CommonModule, ChipsComponent, SkillLevelPipe],
+  imports: [CommonModule, ReactiveFormsModule, ChipsComponent, SkillLevelPipe, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './skills.html',
   styleUrl: './skills.scss'
 })
 export class Skills {
 
+  searchTerm = new FormControl('');
+
   frontendSkills = signal(FRONT_END_SKILLS);
 
   technologies = signal(TECHNOLOGIES);
+
+  constructor() {
+    this.searchTerm.valueChanges.pipe(
+      startWith(''),
+    ).subscribe((term: string | null) => {
+      const filteredTechnologies = TECHNOLOGIES.filter((tech) =>
+        tech.toLowerCase().includes((term ?? '').toLowerCase())
+      );
+      this.technologies.set(filteredTechnologies);
+    });
+  }
+
+  clearSearch(): void {
+    this.searchTerm.setValue('');
+  }
 
 }
