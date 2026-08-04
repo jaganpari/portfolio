8a0470597b2fa6819b5a65efc393481dbff498f5
jaganpari <jaganpari@gmail.com>
2026-08-04 09:32:02 +0530

    search is updated using signal

--- src/app/sections/skills/skills.ts
+++ src/app/sections/skills/skills.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FRONT_END_SKILLS, TECHNOLOGIES } from '../../core/constants/skills.constants';
 import { ChipsComponent } from '../../shared/ui/chips-component/chips-component';
@@ -6,37 +6,32 @@ import { SkillLevelPipe } from '../../shared/pipes/skill-level-pipe';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule, FormControl } from '@angular/forms';
-import { startWith } from 'rxjs';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ChipsComponent, SkillLevelPipe, MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ChipsComponent, SkillLevelPipe, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './skills.html',
   styleUrl: './skills.scss'
 })
 export class Skills {
 
-  searchTerm = new FormControl('');
+  searchTerm = signal('');
 
   frontendSkills = signal(FRONT_END_SKILLS);
 
   technologies = signal(TECHNOLOGIES);
 
-  constructor() {
-    this.searchTerm.valueChanges.pipe(
-      startWith(''),
-    ).subscribe((term: string | null) => {
-      const filteredTechnologies = TECHNOLOGIES.filter((tech) =>
-        tech.toLowerCase().includes((term ?? '').toLowerCase())
-      );
-      this.technologies.set(filteredTechnologies);
-    });
-  }
+  filteredTechnologies = computed(() => {
+    const term = this.searchTerm();
+    return this.technologies().filter((tech) =>
+      tech.toLowerCase().includes(term.toLowerCase())
+    );
+  });
 
   clearSearch(): void {
-    this.searchTerm.setValue('');
+    this.searchTerm.set('');
   }
 
 }
