f70a5c943b9297c1eb1fcca1b3e387c76bd2d4b3
jaganpari <jaganpari@gmail.com>
2026-09-23 07:54:28 +0530

    Skills changes

--- src/app/core/constants/about.constants.ts
+++ src/app/core/constants/about.constants.ts
@@ -1,8 +1,8 @@
 export const HIGHLIGHTED_TECHNOLOGIES: string[] = [
-  'Angular JS, Angular 4 - Angular 19',
-  'React 17-19, JavaScript (ES6+), TypeScript',
+  'Angular JS, Angular 19, React 19',
+  'JavaScript, TypeScript, Bootstrap',
   'NgRx, RxJS, Nx, HTML5, CSS3, SCSS',
-  'Angular Material, Bootstrap, Ant Design',
+  'Angular Material, MUI, Ant Design',
   'Signals, Akita, Redux, Context API',
   'Jest, Jasmine + Karma, Vitest'
 ];
